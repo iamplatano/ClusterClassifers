5bdfa41c36cdc7b127a719058ef0d70c75c7b6c0
Ulises Almonte <ulisesalmonte@gmail.com>
2022-04-12 02:16:16 -0400

    finished project

--- CS484/HW2_almonte/src/drugClassifier.py
+++ CS484/HW2_almonte/src/drugClassifier.py
@@ -1,24 +1,19 @@
-from itertools import count
-from os import remove
-import matplotlib.pyplot as plt
 import numpy as np
-import pandas as pd
 import re
 from sklearn.tree import DecisionTreeClassifier # Import Decision Tree Classifier
-from sklearn.model_selection import train_test_split # Import train_test_split function
+from sklearn.model_selection import cross_validate, train_test_split # Import train_test_split function
 from sklearn import feature_selection, metrics #Import scikit-learn metrics module for accuracy calculation
-from sklearn.feature_selection import VarianceThreshold # Import feature selection features
-from sklearn import tree
 from sklearn.model_selection import cross_val_score
+from sklearn.naive_bayes import BernoulliNB
 
 
 train_matrix = np.zeros((800,100001)) # create zero filled train_matrix
 test_matrix = np.zeros((350,100001))
 
-train_file = open('train_drugs.txt','r')
-test_file = open('test.txt','r')
-format_file = open('formatfile.txt','r')
-predictions = open('predictions','w')
+train_file = open('../train_drugs.txt','r')
+test_file = open('../test.txt','r')
+
+
     
 # Read train file
 lines = train_file.readlines()
@@ -47,19 +42,12 @@ for row in range(len(test_lines)): # cleans each line from file, splits it into 
         cur_val = int(cur_line[col])
         test_matrix[row][cur_val] = 1
 
-# Read test labels from test file
-test_labels_lines = format_file.readlines()
-test_label_matrix = []
-for i in range(len(test_labels_lines)):
-    test_label_matrix.append(int(test_labels_lines[i]))
-test_label_matrix = np.reshape(test_label_matrix,(350,1))
-
 
 # Build Decision Tree Classifier and fit using binary train_matrix and train_matrix_labels
 dtc = DecisionTreeClassifier()
 dtc = dtc.fit(train_matrix,train_matrix_labels)
 
-# Feature selection is based on the importance
+# Feature selection is based on the Gini importance
 importance = dtc.feature_importances_
 important_features = {}
 for i,v in enumerate(importance):
@@ -71,7 +59,7 @@ train_matrix = np.delete(train_matrix, [x for x in range(1000001) if(x not in im
 test_matrix = np.delete(test_matrix, [x for x in range(1000001) if(x not in important_features) ],1)
 
 # Training data set is split 1:4 ratio of testing to training
-x_train, x_test, y_train,y_test= train_test_split(train_matrix,train_matrix_labels,random_state=8 )
+x_train, x_test, y_train,y_test= train_test_split(train_matrix,train_matrix_labels,random_state=8,test_size=0.2)
 
 # Computes an dictionary consisting keys [ccp_alpha,impurities] each with a list of values 
 path = dtc.cost_complexity_pruning_path(x_train,y_train)
@@ -79,37 +67,45 @@ path = dtc.cost_complexity_pruning_path(x_train,y_train)
 ccp_alphas, impurities = path.ccp_alphas, path.impurities
 
 best_ccp_alpha = 0.0
-max_test_score= 0.0
+best_score= 0.0
 
 # Builds trees using a cpp_alpha value and tests against the testing file matrix using F1 scores
 for ccp_alpha in ccp_alphas:
     dtc = DecisionTreeClassifier(random_state=0, ccp_alpha=ccp_alpha)
     dtc.fit(x_train,y_train)
-    
     train_prediction = dtc.predict(x_train)
-    train_prediction = list(map(int,train_prediction))
-    test_prediction = dtc.predict(test_matrix)
-    test_prediction = list(map(int,test_prediction))
-    y_train = list(map(int,y_train))
     y_test = list(map(int,y_test))
+    test_data_cross_val = cross_val_score(dtc,x_test,y_test,cv=10,scoring='f1')
+    test_data_cross_val = list(map(float,test_data_cross_val))
     
-    test_labels_lines = list(map(int,test_labels_lines))
-    train_score = metrics.f1_score(y_train,train_prediction,average = 'binary')
-    test_score = metrics.f1_score(test_label_matrix,test_prediction,average = 'binary')
-    # Records the best cpp_alpha value and writes predictions to file 
-    if (test_score > max_test_score) and (ccp_alpha > 0.0):
+
+    if np.average(test_data_cross_val) > best_score and ccp_alpha > 0.0:
         best_ccp_alpha = ccp_alpha
-        max_test_score = test_score
-        predictions_file = open('predictions_file.txt','w')
-        for line in test_prediction:
-            predictions_file.write('{}\n'.format(str(line)))
+        best_score = np.average(test_data_cross_val)
+
+        
+
+print('Best Cross-Validation Score {} with alpha {}'.format(best_score, best_ccp_alpha))
+
+# Build a new Tree and record predictions onto a file
+dtc = DecisionTreeClassifier(ccp_alpha= best_ccp_alpha)
+dtc.fit(train_matrix,train_matrix_labels)
+predictions = dtc.predict(test_matrix)
+predictions_file = open('../predictions_file.txt','w')
+for i in predictions:
+    predictions_file.writelines(i)
+    predictions_file.writelines('\n')
     
 
-# Print average cross validation scores with the best possible ccp_alpha
-print('best ccp_alpha:{}'.format(best_ccp_alpha))
-train_data_cross_val = cross_val_score(DecisionTreeClassifier(ccp_alpha=best_ccp_alpha),train_matrix,train_matrix_labels)
-print('Train data score: ',np.average(train_data_cross_val))
-test_data_cross_val = cross_val_score(DecisionTreeClassifier(ccp_alpha=best_ccp_alpha),test_matrix,test_label_matrix,cv=10)
-print('Test data score: ',np.average(test_data_cross_val))
+######### Naive Bayes Classifier
+nbc = BernoulliNB() # Initializing Classifier
+bayes_X_train, bayes_X_test, bayes_y_train, bayes_y_test = train_test_split(train_matrix, train_matrix_labels, test_size=0.2, random_state=0)
+nbc.fit(bayes_X_train,bayes_y_train)
+nbc_pred = nbc.predict(bayes_X_test)
 
-predictions_file.close()
+bayes_y_test = list(map(int,bayes_y_test))
+print('bayes classifier: ',np.average(cross_val_score(BernoulliNB(),bayes_X_test,bayes_y_test,cv=10,scoring='f1')))
+
+# print(nbc.feature_log_prob_)
+
+# predictions_file.close()
